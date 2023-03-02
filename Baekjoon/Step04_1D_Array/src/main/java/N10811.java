3d14e553b9793703749a2a8af7746711f7f443c9
JeongJiAn <devjian0219@gmail.com>
2023-03-02 13:19:22 +0900

    Step05_String N11654, N2743

--- Baekjoon/Step04_1D_Array/src/main/java/N10811.java
+++ Baekjoon/Step04_1D_Array/src/main/java/N10811.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class N10811 {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt(), M = scan.nextInt();
+        int[] basckets = new int[N];
+        for (int i = 0; i < N; i++) {
+            basckets[i] = i + 1;
+        }
 
+        for (int i = 0; i < M; i++) {
+            int a = scan.nextInt() - 1, b = scan.nextInt() - 1;
+            for (int j = a; j <= (a + b) / 2; j++) {
+                if (j > b + a - j) {
+                    continue;
+                }
+                int temp = basckets[j];
+                basckets[j] = basckets[b + a - j];
+                basckets[b + a - j] = temp;
+            }
+        }
+        for (int j = 0; j < N; j++) {
+            System.out.printf("%d ", basckets[j]);
+        }
     }
 }
---
+++ Baekjoon/Step05_String/src/main/java/N11654.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class N11654 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char input = scan.nextLine().charAt(0);
+        System.out.printf("%d\n", (int)input);
+    }
+}
