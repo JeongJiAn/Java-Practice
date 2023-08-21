e23f0d4900d4d979aec93c1b2962ed8a3e706d1c
JeongJiAn <devjian0219@gmail.com>
2023-08-21 10:25:21 +0900

    N26069

--- Baekjoon/Step20_Advanced2/src/main/java/N26069.java
+++ Baekjoon/Step20_Advanced2/src/main/java/N26069.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class N26069 {
@@ -7,10 +8,19 @@ public class N26069 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> dancing = new HashMap<>();
+        dancing.put("ChongChong", 1);
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String A = st.nextToken(), B = st.nextToken();
-            
+            if (dancing.containsKey(A) && !dancing.containsKey(B)) {
+                dancing.put(B, 1);
+            } else if (!dancing.containsKey(A) && dancing.containsKey(B)) {
+                dancing.put(A, 1);
+            }
         }
+        bw.write(Integer.toString(dancing.size()) + "\n");
+        bw.flush();
+        bw.close();
     }
 }
